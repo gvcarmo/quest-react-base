b12cc285da06064bb29ef62ae1c8ce1f334e9ce4
gvcarmo <gvittorazze@icloud.com>
2024-10-21 08:43:45 -0300

    Correção feita no componente button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,10 @@
-const showLabel = (label) => {
-    window.alert('A label deste botão é '+(label))
-}
 
 const Button = (props) => {
     return (
         <div>
             <h3>Desafio 2</h3>
             <p>Clique no botão abaixo para mostrar sua Label</p>
-            <button onClick={()=> showLabel(props.label)}>Clique Aqui</button>
+            <button onClick={()=> alert('A label deste botão é '+(props.label))}>Clique Aqui</button>
         </div>
     )
 }
